e61bfb3e5498d1ffad501d641beaf40ebea985b4
Olivier <leplus.olivier@gmail.com>
2021-02-01 21:22:06 +0100

    chore(data): Update API data

--- api/GetTodoList/index.js
+++ api/GetTodoList/index.js
@@ -1,10 +1,10 @@
 module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
     const responseMessage =  [
-         { Title : "Get up", IsCompleted : false},
-         { Title : "Take breakfast", IsCompleted : false},
-         { Title : "Read emails", IsCompleted : true},
-         { Title : "Go back to bed", IsCompleted : false},
+         { Title : "Get up (not too early)", IsCompleted : false},
+         { Title : "Take a good breakfast (hot chocolate please ☕)", IsCompleted : false},
+         { Title : "Read emails (just the important ones)", IsCompleted : true},
+         { Title : "Go back to bed (it's almost nap time)", IsCompleted : false},
     ];
  
     context.res = {
