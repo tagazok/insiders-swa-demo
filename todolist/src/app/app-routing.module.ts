e5788a4b9de65089c3758f6f847266a9070e0f4e
Olivier <leplus.olivier@gmail.com>
2021-02-01 13:26:39 +0100

    feat(todo): Add todo fetch

--- todolist/src/app/app-routing.module.ts
+++ todolist/src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { TodoListComponent } from './todo-list/todo-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    component: TodoListComponent,
+    path: '',
+  },
+  {
+    component: TodoListComponent,
+    path: 'todolist',
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ todolist/src/app/todo-list/todo-list.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-todo-list',
+  templateUrl: './todo-list.component.html',
+  styleUrls: ['./todo-list.component.scss']
+})
+export class TodoListComponent implements OnInit {
+
+  todos: any;
+  constructor(private todoService: TodoService) {}
+ 
+  ngOnInit(): void {
+    this.todoService.getTodoList().then((response) => response.json()).then(data =>{
+       this.todos = data;
+       console.log(data);
+    });
+  }
+
+}
