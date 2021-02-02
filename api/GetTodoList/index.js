0e91182ad379aa2a5463e7008506f0786cd92d32
Olivier <leplus.olivier@gmail.com>
2021-02-02 02:31:20 +0100

    Remove github actions

--- api/GetTodoList/index.js
+++ api/GetTodoList/index.js
@@ -1,5 +1,6 @@
 module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
+    
     const responseMessage =  [
          { Title : "Get up (not too early)", IsCompleted : false},
          { Title : "Take a good breakfast (hot chocolate please ☕)", IsCompleted : false},
